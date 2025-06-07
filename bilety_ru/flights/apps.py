21ed327c518445046bea53147f91da4c237d7bc4
leonidgerasim <porovozov.987654321@mail.ru>
2025-06-07 09:56:07 +0300

    Добален функционал переодического удаления данных flightrequest

--- bilety_ru/flights/apps.py
+++ bilety_ru/flights/apps.py
@@ -5,8 +5,8 @@ class FlightsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'flights'
 
-    def ready(self):
-        # if not os.environ.get('RUN_MAIN'):
-        #     return
-        from . import tasks
-        tasks.DeleteReq().start_scheduler()
+    # def ready(self):
+    #     # if not os.environ.get('RUN_MAIN'):
+    #     #     return
+    #     from . import tasks
+    #     tasks.DeleteReq().start_scheduler()
